cf212e95136fbef5310fd679a193f86f112bc274
Sean Malowney <malowney@gmail.com>
2019-06-30 11:52:23 -0700

    Add files via upload

---
+++ www/data/Address.class.php
@@ -0,0 +1,27 @@
+<?php 
+/*******************************************************************************
+* COPYRIGHT (C) 726 TECHNOLOGY INC, 2017 - 2019            ALL RIGHTS RESERVED *
+*******************************************************************************/
+class Address {    
+    public $sql = "SELECT ADDRESS.ADDRESS_ID, 
+                          ADDRESS.ADDRESS_NAME, ADDRESS.ADDRESS_STREET_1, 
+                          ADDRESS.ADDRESS_STREET_2, ADDRESS.ADDRESS_CITY, 
+                          ADDRESS.ADDRESS_STATE_ID, ADDRESS.ADDRESS_POSTAL 
+                          FROM ADDRESS 
+                          WHERE ADDRESS.ADDRESS_ID = :id";
+    
+    public $object;
+    
+    public function __construct($id) {  
+        try {
+            $conn = new PDO(DB_DSN, DB_USERNAME, DB_PASSWORD);
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $stmt = $conn->prepare($this->sql);
+            $stmt->bindValue("id", $id, PDO::PARAM_INT);
+            $stmt->execute();
+            $this->object = $stmt->fetch();
+        } catch (Exception $ex) {
+            error_log($ex->getMessage());
+        }        
+    }
+}
