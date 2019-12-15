0c3fdb35d2b54dcfc6636a0ad4ecc64c66a1f54d
Sean <malowney@gmail.com>
2019-12-14 23:41:18 -0800

    Added SQL

---
+++ sql/data/UOM.sql
@@ -0,0 +1,9 @@
+INSERT INTO `UOM` (`UOM_ID`, `UOM_SYM`, `UOM_NAME`, `UOM_IS_METRIC`) VALUES
+(1, 'ml', 'milliliters', 1),
+(2, 'g', 'grams', 1),
+(3, 'lb', 'pounds', 0),
+(4, 'oz', 'ounces', 0),
+(5, 'kg', 'kilograms', 1),
+(6, 'm', 'meters', 1),
+(7, 'in', 'inches', 0),
+(8, 'ft', 'feet', 0);
